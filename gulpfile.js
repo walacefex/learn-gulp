392493bdb3643cf07a31fcbf8f5a461a5d2a57ee
walacefex <walacefex01@gmail.com>
2023-01-25 01:23:30 -0300

    Configuration file to automate sass and autoprefixer

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer')
+
+
+function compilaSass() {
+  return gulp.src('scss/*.scss')
+  .pipe(sass({ outputStyle: 'compressed'}))
+  .pipe(autoprefixer({
+    overrideBrowserslist: ['last 2 versions'],
+    cascade: false
+  }))
+  .pipe(gulp.dest('css/'));
+}
+
+gulp.task('default', compilaSass);
